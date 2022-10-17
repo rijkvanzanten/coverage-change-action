777dc9427b8a9b6cac0560f376918cf30232be64
rijkvanzanten <rijkvanzanten@me.com>
2022-10-17 10:32:58 -0400

    Add some todo tests so i don't forget my own ideas

--- index.test.js
+++ index.test.js
@@ -1,3 +1,11 @@
 import { test, expect } from "vitest";
 
-test.todo("It works");
+// This should probably go in a separate lib file:
+test.todo("Glob finds all clover.xml files");
+test.todo("Reads all xml files");
+test.todo("Parses xml to json");
+test.todo("Merges all clover json to single output of file data");
+test.todo("Filters down based on files that were changed in PR");
+
+// - Then do the same thing ^ on main
+// - then create output comment
