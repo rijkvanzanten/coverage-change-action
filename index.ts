88839ec5848d0f9c253942f20829a9ea353a91b0
rijkvanzanten <rijkvanzanten@me.com>
2022-10-18 19:59:54 -0400

    Install deps

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { setFailed, setOutput } from "@actions/core";
 import { findCloverFiles } from "./lib/find-clover-files.js";
+import { installDependencies } from "./lib/install-dependencies.js";
 import { runTests } from "./lib/run-tests.js";
 
 try {
+	await installDependencies();
 	await runTests();
-	await findCloverFiles();
-	setOutput("report", "hello");
+	const files = await findCloverFiles();
+	setOutput("report", files.join(", "));
 } catch (error: unknown) {
 	setFailed(error as string | Error);
 }
